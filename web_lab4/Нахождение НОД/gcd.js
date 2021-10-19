ff74e816e1ced3b9983992fcd573f5c053d3d55d
BreadOwl <71919494+BreadOwl@users.noreply.github.com>
2021-10-19 06:30:10 +0300

    Add files via upload

--- web_lab4/Нахождение НОД/gcd.js
+++ web_lab4/Нахождение НОД/gcd.js
@@ -1,13 +1,18 @@
 function gcd(a, b) {
-    if (a == 0)
-        return b;
-    while (b != 0) {
-        if (a > b)
-            a = a - b;
-        else
-            b = b - a;
+  if (a == 0){
+    return b;
+  }
+  
+  while (b != 0) {
+    if (a > b){
+	  a = a - b;
+	}
+    else{
+      b = b - a;
     }
-    return a;
+  }
+  
+ return a;
 }
 
-console.log(gcd(0, 5)); //для проверки
+console.log( gcd(0, 5) ); //для проверки
