10a3493a0af2fcc118de83e71ee7435b70178f11
BreadOwl <71919494+BreadOwl@users.noreply.github.com>
2022-01-14 17:56:45 +0300

    Add files via upload

---
+++ web_exam/main.js
@@ -0,0 +1,26 @@
+
+function renderRecords(records) {
+    let deliveryServices = document.querySelector('.delivery-services');
+    deliveryServices.innerHTML = '';
+    for (let i = 0; i < records.length; i++) {
+        deliveryServices.append(createListItemElement(records[i]));
+    }
+    console.log(deliveryServices);
+}
+
+function downloadData() {
+    let deliveryServices = document.querySelector('.delivery-services');
+    let url = new URL(factsList.dataset.url);
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', url);
+    xhr.responseType = 'json';
+    xhr.onload = function() {
+        renderRecords(this.response.records);
+    }
+    xhr.send();
+}
+
+
+window.onload = function() {
+    downloadData();
+}
