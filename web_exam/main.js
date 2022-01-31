c879c7469234d65ccceb0d8bae1fa5cc054694ae
BreadOwl <71919494+BreadOwl@users.noreply.github.com>
2022-02-01 00:53:44 +0300

    Add files via upload

--- web_exam/main.js
+++ web_exam/main.js
@@ -1,16 +1,22 @@
 'use strict';
 
+function dataOutput(estabData) {
+    for (let i = 0; i < estabData.length; i++) {
+        estabList.append(estabData[i]);
+        document.getElementById('name').innerHTML = data.name;
+        document.getElementById('typeObject').innerHTML = data.typeObject;
+        document.getElementById('address').innerHTML = data.job;
+    }
+}
+
 async function downloadData() {
-    let estabList = document.querySelector('.estab-list');
     let url = "http://exam-2022-1-api.std-900.ist.mospolytech.ru/api/restaurants?api_key=584ac04a-bca6-4605-8670-7d0b864deda8";
     try {
         const response = await fetch(url);
-        const estabData = await response.json();
+        const estabData = await response.json(); //преобразование данных
         console.log(estabData);
         estabList.innerHTML = '';
-        for (let i = 0; i < estabData.length; i++) {
-            estabList.append(estabData[i]);
-        }
+        dataOutput(estabData);
     } catch (error) { //проверка ошибок
         console.log(error);
     }
@@ -18,5 +24,4 @@ async function downloadData() {
 
 window.onload = function() {
     downloadData();
-
 }
